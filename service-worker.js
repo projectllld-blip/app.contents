1f61f40e06d82cabe61c13b18c1fa37d452b6b7b
Tetsuya Kawaoka <project.llld@gmail.com>
2026-06-13 19:19:49 +0900

    右ダッシュボードの可変幅表示を追加

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-/* SeatFlow PWA Service Worker v1.0.2 */
-const CACHE_VERSION = 'seatflow-pwa-v3';
+/* SeatFlow PWA Service Worker v1.0.3 */
+const CACHE_VERSION = 'seatflow-pwa-v4';
 const CACHE_NAME = CACHE_VERSION;
 
 const urlsToCache = [
